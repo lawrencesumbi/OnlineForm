5966e6bdc07710af3682abb1f0de718b77eda0f2
lawrencesumbi <guiansumbi@gmail.com>
2026-01-26 22:24:44 +0800

    landingv2

--- landing.php
+++ landing.php
@@ -6,6 +6,7 @@
     <title>Landing Page</title>
 </head>
 <body>
+    
     <div class="main">
         <div class="title-con"> 
             <h2>Welcome to</h2>
@@ -23,24 +24,16 @@
 
 <style>
 html, body{margin: 0; width: 100%; height: 100%; overflow: hidden; font-family: 'Inter', sans-serif;}
-.main{position: relative; width: 100vw; height: 100vh; overflow: hidden; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; color: #ffffff; 
-background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), 
-                url('img/landingPhoto.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-}
-
-
+.main{position: relative; width: 100vw; height: 100vh; overflow: hidden; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; color: #ffffff; background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('img/landingPhoto.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;}
 .title-con{width: 900px; margin-bottom: 20px; padding: 0 20px;}
 .title-con h2{font-size: 30px; font-weight: 400; opacity: 0.9;}
 .title-con h1{font-size: 60px; font-weight: 700; margin: 20px 0; font-family: 'Poppins', sans-serif; text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);}
 .title-con h3{font-size: 20px; font-weight: 400; opacity: 0.9; margin-top: 25px;}
 .buttons-con {display: flex; gap: 12px; margin-top: 10px;}
-.buttons-con input {padding: 14px 16px; font-size: 16px; width: 240px; height: 15px; border-radius: 6px;}
-.buttons-con button {padding: 14px 28px; font-size: 16px; border: none; border-radius: 6px; background-color: #1e94d8; color: #fff; cursor: pointer; font-weight: 600; transition: background 0.3s ease;}
+.buttons-con input {padding: 14px 16px; font-size: 15px; border: none; width: 220px; height: 15px; border-radius: 6px;}
+.buttons-con button {padding: 12px 16px; font-size: 16px; border: none; border-radius: 6px; background-color: #1e94d8; color: #fff; cursor: pointer; font-weight: 600; transition: background 0.3s ease;}
 .buttons-con button:hover {background-color: #0d6de3;}
-
 </style>
+
 </body>
 </html>
